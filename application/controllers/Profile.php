9e7318bf27f5e29216e1a5283977d28a72287d47
Francis-PC\Francis <francis94c@gmail.com>
2017-08-19 16:30:15 +0100

    Started Profile Page

---
+++ application/controllers/Profile.php
@@ -0,0 +1,19 @@
+<?php
+class Profile extends CI_Controller {
+
+  function __construct() {
+    parent::__construct();
+    $this->load->helper("url");
+  }
+
+  function index() {
+    $this->load->view("header");
+    $this->load->view("navigation");
+    $this->load->view("content-start");
+    $this->load->view("profile");
+    $this->load->view("content-end");
+    $this->load->view("footer");
+  }
+
+}
+?>
